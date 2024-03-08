0e2a9e333a1b2dcc1025335d197ffb8757b586ca
David Rodrigues <david.proweb@gmail.com>
2024-03-08 17:55:19 -0300

    feat: added debugBenchmark();

--- dist/Debugger.d.ts
+++ dist/Debugger.d.ts
@@ -1,2 +1,9 @@
-import { Command } from "commander";
+import type { Command } from "commander";
 export declare function debugCommander(program: Command, argv: string[]): void;
+interface DebugBenchmarkData<T> {
+    average: number;
+    minimum: number;
+    result: T;
+}
+export declare function debugBenchmark<T>(callback: () => T, printerIn?: ({ average, minimum, result }: DebugBenchmarkData<T>) => void, samplesIn?: number, loopsIn?: number, maxVariance?: number): void;
+export {};
--- src/Debugger.ts
+++ src/Debugger.ts
@@ -1,4 +1,4 @@
-import { Command } from "commander";
+import type { Command } from "commander";
 
 export function debugCommander(program: Command, argv: string[]) {
   program.exitOverride();
@@ -8,4 +8,63 @@ export function debugCommander(program: Command, argv: string[]) {
   });
 
   program.parse(["node", "dummy.js", ...argv]);
+}
+
+interface DebugBenchmarkData<T> {
+  average: number;
+  minimum: number;
+  result: T;
+}
+
+export function debugBenchmark<T>(
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  callback: () => T,
+  printerIn?: ({ average, minimum, result }: DebugBenchmarkData<T>) => void,
+  samplesIn = 1000,
+  loopsIn = Number.POSITIVE_INFINITY,
+  maxVariance = 0.01,
+) {
+  const printer =
+    printerIn ??
+    (({ average, minimum, result }) => {
+      // eslint-disable-next-line no-console
+      console.log(
+        {
+          loops,
+          minimum,
+          average,
+        },
+        result,
+      );
+    });
+
+  let loops = 0;
+  let average: number | undefined;
+  let minimum = Number.POSITIVE_INFINITY;
+
+  while (loops++ < loopsIn) {
+    const samples = Math.min(samplesIn, loops);
+    const now = performance.now();
+
+    const result = callback();
+
+    if (average === undefined) {
+      average = performance.now() - now;
+    } else {
+      const performanceNow =
+        (average / samples) * (samples - 1) +
+        (performance.now() - now) / samples;
+
+      average = Math.max(
+        performanceNow * (1 - maxVariance),
+        Math.min(performanceNow * (1 + maxVariance), performanceNow),
+      );
+    }
+
+    if (average < minimum) {
+      minimum = average;
+    }
+
+    printer({ average, minimum, result });
+  }
 }
